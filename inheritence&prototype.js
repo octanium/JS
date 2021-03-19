9c2057706fe52003c2f8861191940361de387d9d
Mohd Kamran <mohd.kamran@tothenew.com>
2021-03-19 14:55:31 +0530

    JS Classess

--- inheritence&prototype.js
+++ inheritence&prototype.js
@@ -9,9 +9,11 @@ function fun () { // General convention is to start name of function-constructor
 
 fun.prototype.prop2=30;
 fun.prototype.prop3=40;
+fun.prototype.display = () => { console.log('Hii'); }
 
 // new keyword indicates to javascript that execute fun function as a constructor function
 const obj = new fun(); // creating object using function constructor, the new keyword creates a brand new object and takes care that the this keyword in line 3 and 4 must not point to the global and should point to the newly created object, otherwise the this by default points to global Object
+// The new keyword also creates a constructor function inside __proto__ for obj
 //__proto__ is inherited prototype of newly created instance eg obj here,whereas prototype is of function constructor
 console.log(obj.__proto__ === fun.prototype);
 console.log('op...', obj.prop1, obj.prop2, obj.prop3, fun.prototype.constructor.name);
@@ -40,8 +42,10 @@ let fun2 = function () {
 }
 fun2.prototype = fun.prototype; // Works, but a bad Idea
 fun2.prototype = Object.create(fun.prototype); // Better Idea, Reason:Becuase if we use above assignment and wanted to have a different prop3 for fun2, then it will overwrite the prop3 of fun as well
+fun2.prototype.prop5 = 80;
+fun2.display2 = () => { console.log('Bye'); };
 let instanceOffun2 = new fun2();
-console.log('9090', instanceOffun2.prop4, instanceOffun2.prop3, instanceOffun2.prop2, instanceOffun2.prop1); // Now here we are not able to access prop1 because the this at line 39 points to fun2 and not fun
+console.log('9090', instanceOffun2.prop5, instanceOffun2.prop4, instanceOffun2.prop3, instanceOffun2.prop2, instanceOffun2.prop1); // Now here we are not able to access prop1 because the this at line 39 points to fun2 and not fun
 //To overcome that
 fun2 = function () {
     this.prop4 = 60;
@@ -50,6 +54,34 @@ fun2 = function () {
 instanceOffun2 = new fun2();// Equivalent to this = Object.create(fun2);
 console.log('9090', instanceOffun2.prop1);
 
+//Still there is one issue.
+// The constructor inside __proto__ of instanceOffun2 points to fun instead of fun2
+console.log("const..", instanceOffun2.constructor, obj.__proto__.constructor);
+// To point constructor of instanceOffun2 to fun instead of fun2.
+fun2.prototype.constructor = fun2;
+
+//-----------------------------------------------------------------------------------------------------------
+// Converting Everything above to ES6 classes
+
+class funCopy {
+    constructor () {
+        this.prop1 = 10;
+        this.prop2 = 20;
+        this.prop2 = 30;
+        this.prop3 = 40;
+    }
+    display = () => { console.log('Hii'); } // This function will be added to the prototype of funCopy
+}
+
+class fun2Copy extends funCopy {
+    constructor() {
+        super();// Call the super class constructor. Equivalent to funCopy.call(this);
+        this.prop4 = 60;
+    }
+    display2 = () => { console.log('Bye'); }
+}
+
+const finalObj = new fun2Copy();
 
 
 // Difference between function-constructor and Object.create    
